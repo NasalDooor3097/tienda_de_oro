ffca89f09eadc951be160aa0206ddd28277c8037
NasalDooor3097 <albertogutierrezgarci@gmail.com>
2024-07-24 20:19:47 -0600

    ultima actualizacion viernes 24 de julio del 2024

---
+++ inicio_de_secion.js
@@ -0,0 +1,11 @@
+function ventana_recuperar_password()
+{
+
+    var correo = window.prompt("Ingresa tu correo");
+
+    if (correo != null)
+    {
+        var key = window.prompt("Ingresa los digitos que se enviaron al correo")
+    }
+
+}
